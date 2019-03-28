b9f5dac850b13ac0dbdabe27ce01eabbc525d3e3
zc <zachcaceres@gmail.com>
2019-03-28 22:07:53 +0000

    install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# ------------------------------------------------------------------
+# [FastAI SF Study Group] FastAI Audio Install Script
+# ------------------------------------------------------------------
+
+set -m
+pip install pydub librosa fire --user
+mkdir exp
+sudo apt-get --assume-yes install ffmpeg sox libsox-dev libsox-fmt-all
+git clone https://github.com/pytorch/audio.git torchaudio
+cd torchaudio
+python setup.py install

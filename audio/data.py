33698407eb443b477c3deaa4b58cd5022c673b3b
Harry <himion0@gmail.com>
2019-05-31 19:08:53 +0100

    Fixed cache config

--- audio/data.py
+++ audio/data.py
@@ -177,7 +177,7 @@ class AudioList(ItemList):
         cfg = self.config
         if cfg.use_spectro:
             cache_dir = self.path / cfg.cache_dir
-            s = md5(str(asdict(cfg)) + str(p))
+            s = md5(str(asdict(cfg)) + str(asdict(cfg.sg_cfg)) + str(p))
             image_path = cache_dir/(f"{s}.pt")
             if cfg.cache and not cfg.force_cache and image_path.exists():
                 spectro = torch.load(image_path)

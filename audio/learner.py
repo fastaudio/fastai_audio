4947e4268868709be76f65dddde21b9afea9c30c
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-18 12:34:01 +0000

    add shell audio_learner for future waveform use

--- audio/learner.py
+++ audio/learner.py
@@ -1,5 +1,11 @@
 from .data import *
 
+#wrapper for cnn learner that does nothing, but will be used in the future for handling raw waveform (1D) inputs.
+def audio_learner(data:DataBunch, base_arch:Callable=models.resnet18, metrics=accuracy, **kwargs):
+    '''Wrapper function for fastai learner. Converts head of model to fit one channel input.'''
+    learn = cnn_learner(data, base_arch, metrics=metrics, **kwargs)
+    return learn
+
 def audio_predict(learn, item:AudioItem):
     '''Applies the AudioTransforms to the item before predicting its class'''
     config = learn.data.x.config

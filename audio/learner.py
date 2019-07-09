9da7bba93ce85f42e40473fcbd6834958f96b000
Robert Bracco <rbracco@indiana.edu>
2019-07-09 15:46:36 +0000

    add get_audio_preds

--- audio/learner.py
+++ audio/learner.py
@@ -8,9 +8,19 @@ def audio_learner(data:DataBunch, base_arch:Callable=models.resnet18, metrics=ac
 
 def audio_predict(learn, item:AudioItem):
     '''Applies the AudioTransforms to the item before predicting its class'''
-    al = AudioList([item], path=learn.data.x.path, config=learn.data.x.config).split_none().label_empty()
+    if(isinstance(item, torch.Tensor)): return learn.predict(item.squeeze(0).cpu())
+    al = AudioList([item], path=learn.data.path, config=learn.data.x.config).split_none().label_empty()
     spectro = AudioList.open(al, item.path).spectro
     return learn.predict(spectro)
+
+def get_audio_preds(learn, al:AudioList):
+    al = al.split_none().label_empty()
+    data = [AudioList.open(al, ai[0].path).spectro for ai in al.train]
+    preds = [learn.predict(spectro)[1:] for spectro in progress_bar(data)]
+    grouped = [o for o in zip(*preds)]
+    cats = torch.cat([o.view(1) for o in grouped[0]])
+    tens = torch.cat(grouped[1])
+    return (cats, tens)
 
 def window_predict(learn, item:AudioItem, cfg, hop):
     display(item.spectro)

f066b19f21d92ce2b90716cedba6ee847f39857b
Harry <himion0@gmail.com>
2019-09-07 15:14:57 +0100

    Updated major version to match the fastai

--- setup.py
+++ setup.py
@@ -7,9 +7,8 @@ with open('requirements.txt') as f:
 
 setup(
     name='fastaiaudio',
-    version='0.1',
+    version='1.0.1',
     packages=find_packages(),
     description='Audio data with fastai',
     install_requires=requirements,
-    dependency_links=['git+git://github.com/pytorch/torchaudio@f720aec079'],
 )
